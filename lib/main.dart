32d45c8106905665ed220456a90da36c26525f87
ahmed <ekramy3698@gmail.com>
2026-01-06 18:14:01 +0200

    feat: Add Hive model for notes
    Introduces a `NoteModel` for storing note data using Hive. This model will serve as the structure for notes within the local database.
    
    - Creates `NoteModel` with fields for `title`, `description`, `date`, and `color`.
    - Adds Hive type annotations (`@HiveType`, `@HiveField`) to the model for adapter generation.
    - Includes `part 'note_model.g.dart';` for the generated adapter file.
    - Adds `WidgetsFlutterBinding.ensureInitialized();` to `main.dart` in preparation for initializing Hive.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'notes_app.dart';
+import 'package:hive/hive.dart';
+
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Hive
+  // await Hive.initFlutter();
+
+  // Register Hive Adapters (for custom types)??
   runApp(const MyApp());
 }
 
